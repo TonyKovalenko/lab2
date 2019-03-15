a81362e85ee05048ce672cb2ee377060a88d5b8d
Nadya <Katara98Katara@gmail.com>
2019-03-16 01:39:30 +0200

    Add views

---
+++ src/main/java/com/group4/client/view/LoginView.java
@@ -0,0 +1,27 @@
+package com.group4.client.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+
+public class LoginView extends View{
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Hyperlink registerLink;
+
+    @FXML
+    void onLoginClick() {
+
+    }
+
+}
