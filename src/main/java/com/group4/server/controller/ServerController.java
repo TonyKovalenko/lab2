c8803d15e94c4e5ec39e643926d739ef10d9a2de
TonyKovalenko <insane.z500@gmail.com>
2019-05-21 13:57:32 +0300

    [Код] п.3 Добавлена возможность изменения ip-адреса и порта подключения через xml файл.

--- src/main/java/com/group4/server/controller/ServerController.java
+++ src/main/java/com/group4/server/controller/ServerController.java
@@ -1,10 +1,23 @@
 package com.group4.server.controller;
 
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 
@@ -15,14 +28,15 @@ import java.util.concurrent.*;
  * by using {@link MessageController} class instances.
  *
  * @author Nadia Volyk, Anton Kovalenko
- * @since 05-06-19
  * @see MessageController
+ * @since 05-06-19
  */
 public class ServerController extends Thread {
 
     private static final Logger log = Logger.getLogger(ServerController.class.getName());
     private final int availableProcessors;
-    private final int port;
+    private int port = 8888;
+    private String ipAddress = "localhost";
     private ExecutorService executor;
     private volatile boolean isRunning;
 
@@ -32,7 +46,7 @@ public class ServerController extends Thread {
      * by checking {@link #isRunning} field
      *
      * @return true, if server is running
-     *         false, otherwise
+     * false, otherwise
      */
     public boolean isRunning() {
         return isRunning;
@@ -56,22 +70,97 @@ public class ServerController extends Thread {
      */
     ServerController() {
         availableProcessors = Runtime.getRuntime().availableProcessors();
-        port = 8888;
+        if (validateWithDOM("connection-params.xml")) {
+            try {
+                List<String> connectionParams = getConnectionData("connection-params.xml");
+                ipAddress = connectionParams.get(0);
+                port = Integer.parseInt(connectionParams.get(1));
+            } catch (ParserConfigurationException | SAXException | NumberFormatException | IOException ex) {
+                log.error("Exception during connection data read from file" + ex.getMessage());
+                return;
+            }
+        }
+        log.error("Connection parameters file was not validated.");
     }
 
+    /**
+     * Method to check if xml file is valid according
+     * to internal dtd schema.
+     *
+     * @param xmlFile file, with connection parameters
+     * @return true, if file is valid according to dtd schema
+     * false, otherwise
+     */
+    private boolean validateWithDOM(String xmlFile) {
+
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setValidating(true);
+
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+            documentBuilder.setErrorHandler(new ErrorHandler() {
+                @Override
+                public void warning(SAXParseException exception) throws SAXException {
+                    System.out.println("Warning: " + exception.getMessage());
+                    throw exception;
+                }
+
+                @Override
+                public void error(SAXParseException exception) throws SAXException {
+                    System.out.println("Error: " + exception.getMessage());
+                    throw exception;
+                }
+
+                @Override
+                public void fatalError(SAXParseException exception) throws SAXException {
+                    System.out.println("Fatal error: " + exception.getMessage());
+                    throw exception;
+                }
+            });
+            documentBuilder.parse(xmlFile);
+        } catch (SAXException | ParserConfigurationException | IOException ex) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Method to get connection data elements from a xml file.
+     *
+     * @param inputXmlFile file with the connection data elements inside
+     * @return list with the connection data elements inside.
+     * @throws ParserConfigurationException in case of documentbuilder creation
+     * @throws IOException in case if file cannot be read
+     * @throws SAXException in case of any parsing errors from the file
+     */
+    private List<String> getConnectionData(String inputXmlFile) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(inputXmlFile);
+
+        NodeList connectionParamsList = document.getElementsByTagName("connection-params");
+        Node connectionParamsNode = connectionParamsList.item(0);
+        Element connectionParamElement = (Element) connectionParamsNode;
+
+        String ip = connectionParamElement.getElementsByTagName("ip").item(0).getTextContent();
+        String port = connectionParamElement.getElementsByTagName("port").item(0).getTextContent();
+
+        return new ArrayList<>(Arrays.asList(ip, port));
+    }
 
     /**
      * Method responsible for server sockets opening and connections accepting after server was started.
-     *
+     * <p>
      * Processing starts with creating ServerSocket and the pool of threads,
      * with the count of {@link #availableProcessors}
-     *
+     * <p>
      * Connections are accepted in an infinite loop, which terminates
      * when the server is shutting down.
-     *
+     * <p>
      * Every new connection creates a new socket, which is then passed
      * for handling to {@link MessageController} class instance in a new thread.
-     *
+     * <p>
      * In case of server shutdown, ServerSocket will stop accepting new connections
      * and close itself and the pool of threads after.
      */
